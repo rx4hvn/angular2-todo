fc06237bae081de9e4789c7ad1a9c10dad1152fb
Ashok <rx4hvn@gmail.com>
2017-01-10 04:33:25 +0530

    Updated UI

--- client/app/main.ts
+++ client/app/main.ts
@@ -1,5 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
+import {enableProdMode} from '@angular/core';
+
 
 const platform = platformBrowserDynamic();
+enableProdMode();
 platform.bootstrapModule(AppModule);
